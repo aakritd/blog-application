bde3d776f339e2603d5367345aea97f31898df0a
aakritd <aakritdongol123@gmail.com>
2025-04-29 01:22:45 +0545

    blog detail feature

--- server/api/blog/urls.py
+++ server/api/blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.display_blogs, name='display_blogs')
+    path("", views.display_blogs, name='display_blogs'),
+    path("/blog/<id>", views.blog_detail, name='blog_detail')
 ]
--- server/api/blog/views.py
+++ server/api/blog/views.py
@@ -11,3 +11,13 @@ def display_blogs(request):
     # print(blogs[0]['id'])
     content = {'blogs':blogs}#values() return list of dictionary.
     return render(request, 'blog/index.html', content)
+
+
+def blog_detail(request, id):
+
+    blog = Post.objects.get(id=id)
+    # print(blogs[0])
+    # print(blogs)
+    # print(blogs[0]['id'])
+    content = {'blog':blog}#values() return list of dictionary.
+    return render(request, 'blog/blog.html', content)
